9c661e72045733985ce7b4a389fdfd0891c9fa48
machimorales <info@machimorales.com>
2021-09-18 10:31:17 -0500

    Refactoring the html.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
 import "./App.css";
 
-function App() {
+const App: React.FC = () => {
+  const kbOperators = ["AC", "/", "*", "-", "+", "="];
+  const kbNumbers = ["7", "8", "9", "4", "5", " 6", "1", "2", "3", "0"];
+
   return (
     <div className="App">
       <div className="calculator">
         <div className="screen">4376464+488585-5484844+3838</div>
         <div className="keyboard">
           <div className="numbers">
-            <button>0</button>
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
+            {kbNumbers.map((btn) => {
+              return <button>{btn}</button>;
+            })}
           </div>
           <div className="operators">
-            <button>AC</button>
-            <button>/</button>
-            <button>x</button>
-            <button>-</button>
-            <button>+</button>
-            <button>=</button>
+            {kbOperators.map((btn) => {
+              return <button>{btn}</button>;
+            })}
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default App;
