a09fb3cc7ffb5c40b23a200ef1e2a6c3c369fc7f
machimorales <info@machimorales.com>
2021-09-18 10:05:01 -0500

    Creating HTML markup

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/Calculator/i);
+  const linkElement = screen.getByText(/App/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"><h1>Calculator</h1>></div>;
+  return (
+    <div className="App">
+      <div className="calculator">
+        <div className="screen">Screen</div>
+        <div className="keyboard">
+          <div className="operators">operators</div>
+          <div className="numbers">numbers</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
