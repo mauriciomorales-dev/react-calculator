64022f33a1764b243a45f0c6c90f9a1d76c3e1ea
machimorales <info@machimorales.com>
2021-09-18 10:11:49 -0500

    Working on layout. Adding buttons.

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,26 @@ function App() {
       <div className="calculator">
         <div className="screen">Screen</div>
         <div className="keyboard">
-          <div className="operators">operators</div>
-          <div className="numbers">numbers</div>
+          <div className="operators">
+            <button>AC</button>
+            <button>/</button>
+            <button>x</button>
+            <button>-</button>
+            <button>+</button>
+            <button>=</button>
+          </div>
+          <div className="numbers">
+            <button>0</button>
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+          </div>
         </div>
       </div>
     </div>
