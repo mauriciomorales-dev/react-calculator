f4a7cd3a26b7e9f7af04f2e7c516859f5445addc
machimorales <info@machimorales.com>
2021-09-18 12:05:42 -0500

    Working on Actions definition

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,30 @@ import { useState } from "react";
 import "./App.css";
 
 const App: React.FC = () => {
-  const kbOperators = ["AC", "/", "*", "-", "+", "="];
+  const kbOperators = [
+    { op: "ac", label: "AC" },
+    { op: "divide", label: "/" },
+    { op: "multiply", label: "x" },
+    { op: "substract", label: "-" },
+    { op: "add", label: "+" },
+    { op: "eql", label: "=" },
+  ];
+
   const kbNumbers = ["7", "8", "9", "4", "5", "6", "1", "2", "3", "0"];
 
   const [operation, setOperation] = useState<string>("");
 
-  const resetCalc = () => {
-    setOperation("");
-  };
-
-  const actionExecution = {
-    c: "AC",
-    d: "Divie",
-    m: "Multiply",
-    mn: "Minus",
-    p: "Plus",
-    e: "equal",
+  const actionExecution: any = {
+    ac: "AC",
+    divide: "Divide",
+    multiply: "Multiply",
+    substract: "Substract",
+    add: "Add",
+    eql: "Equal",
   };
 
   const handleKeyboard = (value: string) => {
-    const action = actionExecution["p"];
+    const action = actionExecution[value];
     console.log(action);
     //value === "AC" ? resetCalc() : setOperation(`${operation}${value}`);
   };
@@ -43,10 +47,10 @@ const App: React.FC = () => {
             })}
           </div>
           <div className="operators">
-            {kbOperators.map((btn, index) => {
+            {kbOperators.map((btn) => {
               return (
-                <button key={index} onClick={() => handleKeyboard(btn)}>
-                  {btn}
+                <button key={btn.op} onClick={() => handleKeyboard(btn.op)}>
+                  {btn.label}
                 </button>
               );
             })}
