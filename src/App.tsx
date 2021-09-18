92ebc0294b5e5591169dc2598611fa42e09988aa
machimorales <info@machimorales.com>
2021-09-18 10:47:24 -0500

    Adding click events and planing conditions of how it should work.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,38 @@
+import { useState } from "react";
 import "./App.css";
 
 const App: React.FC = () => {
   const kbOperators = ["AC", "/", "*", "-", "+", "="];
   const kbNumbers = ["7", "8", "9", "4", "5", " 6", "1", "2", "3", "0"];
 
+  const [operation, setOperation] = useState<string>("");
+
+  const handleKeyboard = (value: string) => {
+    console.log(`Click: ${value}`);
+    setOperation(value);
+  };
+
   return (
     <div className="App">
       <div className="calculator">
-        <div className="screen">4376464+488585-5484844+3838</div>
+        <div className="screen">{operation}</div>
         <div className="keyboard">
           <div className="numbers">
-            {kbNumbers.map((btn) => {
-              return <button>{btn}</button>;
+            {kbNumbers.map((btn, index) => {
+              return (
+                <button key={index} onClick={() => handleKeyboard(btn)}>
+                  {btn}
+                </button>
+              );
             })}
           </div>
           <div className="operators">
-            {kbOperators.map((btn) => {
-              return <button>{btn}</button>;
+            {kbOperators.map((btn, index) => {
+              return (
+                <button key={index} onClick={() => handleKeyboard(btn)}>
+                  {btn}
+                </button>
+              );
             })}
           </div>
         </div>
@@ -26,3 +42,37 @@ const App: React.FC = () => {
 };
 
 export default App;
+
+/*
+ESCENARIOS INICIALES:
+ESC1- Display vacío.
+ESC2- Display finalizado en Numero.
+ESC3- Display finalizado en operador matemático (/,*,-,+).
+
+ACCIONES
+A1- Usuario presiona un numero.
+A2- Usuario presiona un operador matemático (/,*,-,+).
+A3- Usuario presiona un "="
+A4- Usuario presiona un "AC"
+
+
+Given ESC1
+When A1
+Then se concatena el VALOR con el valor en el DISPLAY.
+
+Given ESC1
+When A2
+Then se IGNORA.
+
+Given ESC1
+When A1
+Then se concatena el VALOR con el valor en el DISPLAY.
+
+Given ESC1
+When A1
+Then se concatena el VALOR con el valor en el DISPLAY.
+
+Given ESC1
+When A1
+Then se concatena el VALOR con el valor en el DISPLAY.
+*/
